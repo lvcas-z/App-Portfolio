863186edc8864468d0e8076d4907b25c98755d43
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2023-04-08 21:09:25 -0300

    header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './Header.css'
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleVisibility = ()=> setIsOpen(isOpen=>!isOpen)
+
     return (
-    <nav>
+    <header>
         <div className='logo-ctn'>
-            <span>LZ</span>
+            <span className='logo'>LZ</span>
         </div>
-        <div className='nav-links-ctn'>
+        <span onClick={toggleVisibility} className='open-menu'>==</span>
+        <nav className={isOpen ? "open" : ""}>
+            <span onClick={toggleVisibility} className='close-menu'>X</span>
             <ul className='nav-links'>
                 <li className='nav-item'>Inicio</li>
                 <li className='nav-item'>Sobre Mi</li>
                 <li className='nav-item'>Proyectos</li>
                 <li className='nav-item'>Contacto</li>
             </ul>
-        </div>
-    </nav>
+        </nav>
+    </header>
     )
 }
 
