6b9f97dbd798655005f48b04527509058c12d760
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2023-04-20 18:00:43 -0300

    se agregaron las skills y se cambiaron algunos detalles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,17 +3,26 @@ import codingImg from '../../assets/coding.png'
 import './About.css'
 const About = () => {
 return (
-    <>
-        <h2>Sobre mi</h2>
-        <div className='about-ctn'>
+    <div className='about-ctn'>
+        <h3>Sobre mi</h3>
+        <div className='about-me-ctn'>
             <div className='about-img-ctn'>
                 <img src={codingImg} alt=""/>
             </div>
             <div className='about-info-ctn'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ullam atque corrupti hic soluta nihil suscipit. Aperiam optio esse provident at labore, sit suscipit veniam modi, molestias reiciendis expedita rerum.</p>
-                </div>
+            </div>
         </div>
-    </>
+        <div className='skills-ctn'>
+            <h3>Skills</h3>
+            <ul className='skills-items'>
+                <li className='skill-item'>Icon</li>
+                <li className='skill-item'>Icon</li>
+                <li className='skill-item'>Icon</li>
+                <li className='skill-item'>Icon</li>
+            </ul>
+        </div>
+    </div>
 )
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className='hero-ctn'>
             <div className="glowing-ctn">
                 <h1 className="hero-title ">Lucas Zelaya</h1>
-                <h3 className="hero-subtitle">FRONTEND DEVELOPER</h3>
+                <h2 className="hero-subtitle">FRONTEND DEVELOPER</h2>
             </div>
         </div>
     )
