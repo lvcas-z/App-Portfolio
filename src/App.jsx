282f72217ff3718747d173aaad1d41c6d90ddac9
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2023-04-20 22:51:36 -0300

    se creo el componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
@@ -13,6 +14,7 @@ function App() {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   )
 }
