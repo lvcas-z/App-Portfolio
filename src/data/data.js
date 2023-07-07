0224364cf58f2713492e2a6fc8a793d9ada5c4c8
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2023-07-07 14:47:17 -0300

    se agrego el proyecto hoodie store

--- src/data/data.js
+++ src/data/data.js
@@ -1,55 +1,56 @@
 import digitalBooking from '../assets/digitalBooking.png'
 import gifFinder from '../assets/gifFinder.png'
 import footballPlayers from '../assets/footballPlayers.png'
-const projects=[
-        {
-            "id":1,
-            "nombre":"Digital Booking",
-            "url":"",
-            "repositorio":"https://github.com/lvcas-z/App-Portfolio",
-            "img":digitalBooking,
-            "tecnologias":"React, CSS, Java, AWS, MySql"
-        },
-        {
-            "id":2,
-            "nombre":"Gif Finder",
-            "url":"https://lvcas-z.github.io/App-GifFinder/",
-            "repositorio":"https://github.com/lvcas-z/App-GifFinder",
-            "img":gifFinder,
-            "tecnologias":"React, CSS"
-        },
-        {
-            "id":3,
-            "nombre":"Football Players",
-            "url":"https://lvcas-z.github.io/App-FootballPlayers/",
-            "repositorio":"https://github.com/lvcas-z/App-FootballPlayers",
-            "img":footballPlayers,
-            "tecnologias":"Javascript, SASS, HTML"
-        },
-        {
-            "id":4,
-            "nombre":"Próximamente...",
-            "url":"",
-            "repositorio":"",
-            "img":"",
-            "tecnologias":"Próximamente"
-        },
-        {
-            "id":5,
-            "nombre":"Próximamente...",
-            "url":"",
-            "repositorio":"",
-            "img":"",
-            "tecnologias":"Próximamente"
-        },
-        {
-            "id":6,
-            "nombre":"Próximamente...",
-            "url":"",
-            "repositorio":"",
-            "img":"",
-            "tecnologias":"Próximamente"
-        }
-    ]
+import hoodieStore from '../assets/hoodieStore.png'
 
-    export default projects
+const projects = [
+    {
+        "id": 1,
+        "nombre": "Digital Booking",
+        "url": "",
+        "repositorio": "https://github.com/lvcas-z/App-Portfolio",
+        "img": digitalBooking,
+        "tecnologias": "React, CSS, Java, AWS, MySQL"
+    },
+    {
+        "id": 2,
+        "nombre": "Hoodie Store",
+        "url": "https://app-hoodie-store.vercel.app/",
+        "repositorio": "https://github.com/lvcas-z/App-HoodieStore",
+        "img": hoodieStore,
+        "tecnologias": "React, CSS , Firebase"
+    },
+    {
+        "id": 3,
+        "nombre": "Gif Finder",
+        "url": "https://lvcas-z.github.io/App-GifFinder/",
+        "repositorio": "https://github.com/lvcas-z/App-GifFinder",
+        "img": gifFinder,
+        "tecnologias": "React, CSS"
+    }, {
+        "id": 4,
+        "nombre": "Football Players",
+        "url": "https://lvcas-z.github.io/App-FootballPlayers/",
+        "repositorio": "https://github.com/lvcas-z/App-FootballPlayers",
+        "img": footballPlayers,
+        "tecnologias": "Javascript, SASS, HTML"
+    },
+    {
+        "id": 5,
+        "nombre": "Próximamente...",
+        "url": "",
+        "repositorio": "",
+        "img": "",
+        "tecnologias": "Próximamente"
+    },
+    {
+        "id": 6,
+        "nombre": "Próximamente...",
+        "url": "",
+        "repositorio": "",
+        "img": "",
+        "tecnologias": "Próximamente"
+    }
+]
+
+export default projects
